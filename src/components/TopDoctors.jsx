caa6a448e9e20180413c6b43020d8d4c1d1d5940
Dimaaap <procdima49@gmail.com>
2025-10-19 21:17:35 +0300

    Added doctors section

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -1,8 +1,28 @@
+import { doctors } from "../assets/assets_frontend/assets"
+
 const TopDoctors = () => {
     return(
         <div>
             <h1>Top Doctors to Book</h1>
             <p>Simply browse through our extensive list of trusted doctors.</p>
+            <div>
+                { doctors.slice(0, 10).map((item, index) => (
+                    <div key={ index }>
+                        <img src={ item.image } alt="" />
+                        <div>
+                            <div>
+                                <p></p>
+                                <p>
+                                    Available
+                                </p>
+                            </div>
+                            <p>{ item.name }</p>
+                            <p>{ item.speciality }</p>
+                        </div>
+                    </div>
+                )) }
+            </div>
+            <button>more</button>
         </div>
     )
 }
