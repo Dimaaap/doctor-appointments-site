569f1225fe561c0217c5be490fc71f4d958c405b
Dimaaap <procdima49@gmail.com>
2025-10-29 19:23:08 +0200

    Added new components

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -1,7 +1,26 @@
+import { useContext } from "react";
+import { useParams } from "react-router-dom"
+import { AppContext } from "../context/AppContext"
+
 const Doctors = () => {
+
+    const { speciality } = useParams();
+
+    const { doctors } = useContext(AppContext)
+
     return(
         <div>
-            
+            <p>Browse through the doctors specialist.</p>
+            <div>
+                <div>
+                    <p>General physician</p>
+                    <p>Gynecologist</p>
+                    <p>Dermatologist</p>
+                    <p>Pediatricians</p>
+                    <p>Neurologist</p>
+                    <p>Gastroenterologist</p>
+                </div>
+            </div>
         </div>
     )
 }
